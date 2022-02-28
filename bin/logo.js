aaa7eae6f4c66a0146f49676c51413bea1340d65
halodong <drl990114@gmail.com>
2022-02-28 23:31:10 +0800

    feat: support loading own fonts

--- bin/logo.js
+++ bin/logo.js
@@ -1,36 +1,39 @@
 #!/usr/bin/env node
-const { createCanvas } = require('canvas')
 const fs = require('fs')
 const {
   init,
   hasOptionsFile,
-  wirteLogoFile,
-  wirteOptionsFile
+  writeLogoFile,
+  writeOptionsFile,
+  getFnOptions,
+  getFontList,
+  registFont
 } = require('./utils')
-const canvas = createCanvas(400, 400)
-const createCanvasEl = () => createCanvas(400, 400)
+
 const optionsFileName = 'logo.json'
 const hasfile = hasOptionsFile(optionsFileName)
-let options = {
-  canvas,
-  createCanvas: createCanvasEl
-}
+
 if (!hasfile) {
   init().then((inputOptions) => {
-    options = {
-      ...options,
+    const { width, height, fontFamily } = inputOptions
+    registFont(fontFamily)
+    const options = {
+      ...getFnOptions(width, height),
       ...inputOptions
     }
-    wirteOptionsFile(inputOptions)
-    wirteLogoFile(options)
+    writeOptionsFile(inputOptions)
+    writeLogoFile(options)
   })
 } else {
   fs.readFile(optionsFileName, (err, data) => {
     const inputOptions = JSON.parse(Buffer(data).toString())
-    options = {
-      ...options,
+    const { width, height, fontFamily } = inputOptions
+    registFont(fontFamily)
+    const options = {
+      ...getFnOptions(width, height),
       ...inputOptions
     }
-    wirteLogoFile(options)
+    writeLogoFile(options)
   })
 }
+getFontList()
